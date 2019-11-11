85a026cda278230da49f5d649956806568badf4f
adpppkkk <yellowjerry6@gmail.com>
2019-11-11 11:59:20 +0800

    Fix submit

--- app/src/main/java/com/example/madlibs/MainActivity.kt
+++ app/src/main/java/com/example/madlibs/MainActivity.kt
@@ -5,24 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.view.isInvisible
+import android.text.TextUtils
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 class MainActivity : AppCompatActivity() {
+    val player = "player"
+    val word1 = "word_1"
+    val word2 = "word_2"
+    val word3 = "word_3"
+    val word4 = "word_4"
+    val word5 = "word_5"
+    val word6 = "word_6"
+    val word7 = "word_7"
+    val word8 = "word_8"
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    val player = "player"
-    val word1 = "word"
-    val word2 = "word"
-    val word3 = "word"
-    val word4 = "word"
-    val word5 = "word"
-    val word6 = "word"
-    val word7 = "word"
-    val word8 = "word"
+
 
 
 
@@ -37,19 +49,49 @@ class MainActivity : AppCompatActivity() {
         val input7 = findViewById<TextView>(R.id.text7).text.toString()
         val input8 = findViewById<TextView>(R.id.text8).text.toString()
 
-        val intent = Intent(this,libpage::class.java).apply {
-            putExtra(player, playerName)
-            putExtra(word1, input1)
-            putExtra(word2, input2)
-            putExtra(word3, input3)
-            putExtra(word4, input4)
-            putExtra(word5, input5)
-            putExtra(word6, input6)
-            putExtra(word7, input7)
-            putExtra(word8, input8)
 
+        if (playerName.isEmpty()) {
+            findViewById<EditText>(R.id.player)?.error = "PlayerName Needed!"
         }
-        startActivity(intent)
+        if (input1.isEmpty()) {
+            findViewById<EditText>(R.id.text1)?.error = "A word is needed here!"
+        }
+        if (input2.isEmpty()) {
+            findViewById<EditText>(R.id.text2)?.error = "A word is needed here!"
+        }
+        if (input3.isEmpty()) {
+            findViewById<EditText>(R.id.text3)?.error = "A word is needed here!"
+        }
+        if (input4.isEmpty()) {
+            findViewById<EditText>(R.id.text4)?.error = "A word is needed here!"
+        }
+        if (input5.isEmpty()) {
+            findViewById<EditText>(R.id.text5)?.error = "A word is needed here!"
+        }
+        if (input6.isEmpty()) {
+            findViewById<EditText>(R.id.text6)?.error = "A word is needed here!"
+        }
+        if (input7.isEmpty()) {
+            findViewById<EditText>(R.id.text7)?.error = "A word is needed here!"
+        }
+        if (input8.isEmpty()) {
+            findViewById<EditText>(R.id.text8)?.error = "A word is needed here!"
+        }
+        else {
+            val intent = Intent(this,libpage::class.java).apply {
+                putExtra(player, playerName)
+                putExtra(word1, input1)
+                putExtra(word2, input2)
+                putExtra(word3, input3)
+                putExtra(word4, input4)
+                putExtra(word5, input5)
+                putExtra(word6, input6)
+                putExtra(word7, input7)
+                putExtra(word8, input8)
+            }
+            startActivity(intent)
+        }
+
 
     }
 }
--- app/src/main/java/com/example/madlibs/libpage.kt
+++ app/src/main/java/com/example/madlibs/libpage.kt
@@ -7,14 +7,14 @@ import android.widget.TextView
 class libpage : AppCompatActivity() {
 
     val player = "player"
-    val word1 = "word"
-    val word2 = "word"
-    val word3 = "word"
-    val word4 = "word"
-    val word5 = "word"
-    val word6 = "word"
-    val word7 = "word"
-    val word8 = "word"
+    val word1 = "word_1"
+    val word2 = "word_2"
+    val word3 = "word_3"
+    val word4 = "word_4"
+    val word5 = "word_5"
+    val word6 = "word_6"
+    val word7 = "word_7"
+    val word8 = "word_8"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +32,32 @@ class libpage : AppCompatActivity() {
 
 
         val message1 = findViewById<TextView>(R.id.message1).apply{
-            text = "Hello"
+            text = text1
+        }
+        val message2 = findViewById<TextView>(R.id.message2).apply{
+            text = text2
+        }
+        val message3 = findViewById<TextView>(R.id.message3).apply{
+            text = text3
+        }
+        val message4 = findViewById<TextView>(R.id.message4).apply{
+            text = text4
+        }
+        val message5 = findViewById<TextView>(R.id.message5).apply{
+            text = text5
+        }
+        val message6 = findViewById<TextView>(R.id.message6).apply{
+            text = text6
+        }
+        val message7 = findViewById<TextView>(R.id.message7).apply{
+            text = text7
+        }
+        val message8 = findViewById<TextView>(R.id.message8).apply{
+            text = text8
         }
         val playerView = findViewById<TextView>(R.id.playerView).apply {
             text = playerName
         }
     }
 }
+// comment just for submit
